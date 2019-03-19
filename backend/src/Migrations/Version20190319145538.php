1d7a3e0a072a2c68599f90ea029fc19c537b8f79
Live session user <furanku13code@gmail.com>
2019-03-19 16:01:36 +0100

    correction des migrations

--- backend/src/Migrations/Version20190319145538.php
+++ backend/src/Migrations/Version20190319145538.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190319143905 extends AbstractMigration
+final class Version20190319145538 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,15 +22,17 @@ final class Version20190319143905 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FA76ED395');
-        $this->addSql('ALTER TABLE user_user_role DROP FOREIGN KEY FK_2D084B47A76ED395');
+        $this->addSql('CREATE TABLE user_role (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(128) NOT NULL, code VARCHAR(128) NOT NULL, is_active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE app_users (id INT AUTO_INCREMENT NOT NULL, person_id INT NOT NULL, roles JSON NOT NULL, last_connnection DATETIME NOT NULL, INDEX IDX_C2502824217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_user_role (user_id INT NOT NULL, user_role_id INT NOT NULL, INDEX IDX_2D084B47A76ED395 (user_id), INDEX IDX_2D084B478E0E3CA6 (user_role_id), PRIMARY KEY(user_id, user_role_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE app_users ADD CONSTRAINT FK_C2502824217BBB47 FOREIGN KEY (person_id) REFERENCES person (id)');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('ALTER TABLE user_user_role DROP FOREIGN KEY FK_2D084B47A76ED395');
         $this->addSql('ALTER TABLE user_user_role ADD CONSTRAINT FK_2D084B47A76ED395 FOREIGN KEY (user_id) REFERENCES app_users (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FA76ED395');
+        $this->addSql('ALTER TABLE user_user_role ADD CONSTRAINT FK_2D084B478E0E3CA6 FOREIGN KEY (user_role_id) REFERENCES user_role (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE person CHANGE email email VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_34DCD176E7927C74 ON person (email)');
+        $this->addSql('ALTER TABLE company ADD user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES app_users (id)');
+        $this->addSql('CREATE INDEX IDX_4FBF094FA76ED395 ON company (user_id)');
         $this->addSql('ALTER TABLE request ADD contact_id INT NOT NULL');
         $this->addSql('ALTER TABLE request ADD CONSTRAINT FK_3B978F9FE7A1254A FOREIGN KEY (contact_id) REFERENCES contact (id)');
         $this->addSql('CREATE INDEX IDX_3B978F9FE7A1254A ON request (contact_id)');
@@ -41,17 +43,18 @@ final class Version20190319143905 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE user_user_role DROP FOREIGN KEY FK_2D084B478E0E3CA6');
         $this->addSql('ALTER TABLE user_user_role DROP FOREIGN KEY FK_2D084B47A76ED395');
         $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FA76ED395');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, person_id INT NOT NULL, roles JSON NOT NULL, last_connnection DATETIME NOT NULL, INDEX IDX_8D93D649217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649217BBB47 FOREIGN KEY (person_id) REFERENCES person (id)');
+        $this->addSql('DROP TABLE user_role');
         $this->addSql('DROP TABLE app_users');
-        $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FA76ED395');
-        $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('DROP TABLE user_user_role');
+        $this->addSql('DROP INDEX IDX_4FBF094FA76ED395 ON company');
+        $this->addSql('ALTER TABLE company DROP user_id');
+        $this->addSql('DROP INDEX UNIQ_34DCD176E7927C74 ON person');
+        $this->addSql('ALTER TABLE person CHANGE email email VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
         $this->addSql('ALTER TABLE request DROP FOREIGN KEY FK_3B978F9FE7A1254A');
         $this->addSql('DROP INDEX IDX_3B978F9FE7A1254A ON request');
         $this->addSql('ALTER TABLE request DROP contact_id');
-        $this->addSql('ALTER TABLE user_user_role DROP FOREIGN KEY FK_2D084B47A76ED395');
-        $this->addSql('ALTER TABLE user_user_role ADD CONSTRAINT FK_2D084B47A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
 }
