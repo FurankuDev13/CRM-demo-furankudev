449cd474b6f4b16f299dfb5f11e25eb69cccb33e
Philippe P <44428700+Philpagan@users.noreply.github.com>
2019-03-20 10:49:37 +0100

    Starting fixtures done

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,20 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Unirest\Request;
 use App\Entity\Person;
+use App\Entity\Company;
+use App\Entity\Contact;
 use App\Entity\Product;
 use App\Entity\Category;
 use App\Entity\UserRole;
+use App\Entity\ContactType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
@@ -36,7 +41,6 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-
         //Request sur API ontariobeer
         Request::verifyPeer(false);
         $headers = array('Accept' => 'application/json');
@@ -47,7 +51,6 @@ class AppFixtures extends Fixture
             $product = new Product();
             $category = new Category();
             $category->setName($response->body[$i]->category);
-            $category->setIsActive(true);
             $category->setCreatedAt($dateToSet);
             $category->setUpdatedAt($dateToSet);
     
@@ -59,74 +62,98 @@ class AppFixtures extends Fixture
             $product->setMaxDiscountRate(15);
             $product->setIsOnHomePage(true);
             $product->setRank(10);
-            $product->setIsActive(true);
-            $product->setCreatedAt(new \DateTime());
-            $product->setUpdatedAt(new \DateTime());
             
             //dd($this->completeCategory($category));
             $product->addCategory($this->completeCategory($category));
 
             $manager->persist($product);
         }
+
         foreach ($this->categories as $category) {
             $manager->persist($category);
         }
 
+        $company = new Company();
+        $company->setName('Tartenpion SAS');
+        $manager->persist($company);
+
         $salesRole = new UserRole();
         $salesRole->setTitle('Commercial');
         $salesRole->setCode('ROLE_SALES');
-        $salesRole->setIsActive(true);
-        $salesRole->setCreatedAt(new \DateTime());
-        $salesRole->setUpdatedAt(new \DateTime());
         $manager->persist($salesRole);
+
         $salesMgrRole = new UserRole();
         $salesMgrRole->setTitle('Responsable Commercial');
         $salesMgrRole->setCode('ROLE_SALESMGR');
-        $salesMgrRole->setIsActive(true);
-        $salesMgrRole->setCreatedAt(new \DateTime());
-        $salesMgrRole->setUpdatedAt(new \DateTime());
         $manager->persist($salesMgrRole);
+
         $supportRole = new UserRole();
         $supportRole->setTitle('Support');
         $supportRole->setCode('ROLE_SUPPORT');
-        $supportRole->setIsActive(true);
-        $supportRole->setCreatedAt(new \DateTime());
-        $supportRole->setUpdatedAt(new \DateTime());
         $manager->persist($supportRole);
+
         $managerRole = new UserRole();
         $managerRole->setTitle('Responsable Commercial');
         $managerRole->setCode('ROLE_MANAGER');
-        $managerRole->setIsActive(true);
-        $managerRole->setCreatedAt(new \DateTime());
-        $managerRole->setUpdatedAt(new \DateTime());
         $manager->persist($managerRole);
+
         $adminRole = new UserRole();
         $adminRole->setTitle('Administrateur');
         $adminRole->setCode('ROLE_ADMIN');
-        $adminRole->setIsActive(true);
-        $adminRole->setCreatedAt(new \DateTime());
-        $adminRole->setUpdatedAt(new \DateTime());
         $manager->persist($adminRole);
 
-        // $person = new Person();
-        // $person->setFirstname('Franck');
-        // $person->setLastname('TANUKI');
-        // $person->setEmail('franck_t@oclock.io');
-        // $encodedPassword = $this->passwordEncoder->encodePassword($person, 'franck');
-        // $person->setPassword($encodedPassword);
-        // $person->setBusinessPhone('0102030405');
-        // $person->setCellPhone('0102030405');
+        $ownerType = new ContactType();
+        $ownerType->setTitle('Gérant');
+        $manager->persist($ownerType);
 
-        // $person = new Person();
-        // $person->setFirstname('Phil');
-        // $person->setLastname('POIRON');
-        // $person->setEmail('franck_t@oclock.io');
-        // $encodedPassword = $this->passwordEncoder->encodePassword($person, 'phil');
-        // $person->setPassword($encodedPassword);
-        // $person->setBusinessPhone('0102030405');
-        // $person->setCellPhone('0102030405');
+        $person1 = new Person();
+        $person1->setFirstname('Franck');
+        $person1->setLastname('TANUKI');
+        $person1->setBusinessPhone('0102030405');
+        $person1->setCellPhone('0102030405');
+        $manager->persist($person1);
 
+        $person2 = new Person();
+        $person2->setFirstname('Phil');
+        $person2->setLastname('PAGAN');
+        $person2->setBusinessPhone('0102030405');
+        $person2->setCellPhone('0102030405');
+        $manager->persist($person2);
+
+        $person3 = new Person();
+        $person3->setFirstname('PF');
+        $person3->setLastname('ECCHI');
+        $person3->setBusinessPhone('0102030405');
+        $person3->setCellPhone('0102030405');
+        $manager->persist($person3);
+
+        $user1 = new User();
+        $user1->setPerson($person1);
+        $user1->addUserRole($salesRole);
+        $user1->addUserRole($supportRole);
+        $user1->setEmail('franck_t@oclock.io');
+        $encodedPassword = $this->passwordEncoder->encodePassword($user1, 'franck');
+        $user1->setPassword($encodedPassword);
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setPerson($person2);
+        $user2->addUserRole($adminRole);
+        $user2->setEmail('phil_p@oclock.io');
+        $encodedPassword = $this->passwordEncoder->encodePassword($user2, 'phil');
+        $user2->setPassword($encodedPassword);
+        $manager->persist($user2);
+
+        $contact1 = new Contact();
+        $contact1->setPerson($person3);
+        $contact1->setContactType($ownerType);
+        $contact1->setCompany($company);
+        $contact1->setEmail('pf_e@oclock.io');
+        $encodedPassword = $this->passwordEncoder->encodePassword($contact1, 'pf');
+        $contact1->setPassword($encodedPassword);
+        $manager->persist($contact1);
 
         $manager->flush();
+        
     }
 }
