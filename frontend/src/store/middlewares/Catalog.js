685f99942efa208a81d8c59c1229601889232cec
Pierre-François Picolo <pierre-francois.picolo@hotmail.fr>
2019-03-20 17:31:01 +0100

    en l'etat après problèmes requête

--- frontend/src/store/middlewares/Catalog.js
+++ frontend/src/store/middlewares/Catalog.js
@@ -3,9 +3,6 @@
 */
 import axios from 'axios';
 
-const axiosUp = axios.create({
-  baseURL: '',
-});
 /* base URL à définir quand le back sera prêt */
 
 /**
@@ -13,19 +10,29 @@ const axiosUp = axios.create({
 */
 import { FETCH_CATALOG } from 'src/store/reducer';
 
+
+const axiosUp = axios.create({
+  baseURL: 'http://localhost:8001',
+});
+
 // Middleware : ajax : gestion des lettres
 const ajaxCatalog = store => next => (action) => {
   switch (action.type) {
     case FETCH_CATALOG:
       console.log('Je cherche ! je cherche !');
-      /* axiosUp.get('/catalog')
+      axiosUp.get('/api/products', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+        },
+      })
         .then((response) => {
           console.log(response);
-          store.dispatch(fetchSuccess(data)
+          /* store.dispatch(fetchSuccess(data) */
         })
         .catch((error) => {
           console.log(error);
-        }); */
+        });
       break;
 
     default:
