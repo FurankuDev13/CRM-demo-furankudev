0add8ff811853d1c3d3c64925f7dc91cf135845b
Live session user <furanku13code@gmail.com>
2019-03-21 11:36:22 +0100

    new et edit pour Category avec MAJ controller, FormType et templates

--- backend/src/Form/CategoryType.php
+++ backend/src/Form/CategoryType.php
@@ -5,21 +5,61 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('picture')
-            ->add('rank')
-            ->add('isActive')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('products')
+            ->add('name', TextType::class, [
+                'label'    => "Nom de la catégorie",
+                'attr' => [
+                    'placeholder' => "nom",
+                    ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ ne doit pas être vide'
+                    ]),
+                    new Length([
+                        'min'        => 1,
+                        'max'        => 100,
+                        'minMessage' => 'Pas assez de caractères , attendu : {{ limit }}',
+                        'maxMessage' => 'Trop de caractères, attendu: {{ limit }}',
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label'    => "Description de la catégorie",
+                'attr' => [
+                    'placeholder' => "description",
+                    ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ ne doit pas être vide'
+                    ]),
+                    new Length([
+                        'min'        => 1,
+                        'max'        => 300,
+                        'minMessage' => 'Pas assez de caractères , attendu : {{ limit }}',
+                        'maxMessage' => 'Trop de caractères, attendu: {{ limit }}',
+                    ])
+                ]
+            ])
+            ->add('picture', UrlType::class, [
+                'label'    => "Url de l'image",
+                'attr' => ['placeholder' => "url"],
+            ])
+            ->add('rank', IntegerType::class, [
+                'label'    => "Ordre d'affichage de la catégorie",
+                'attr' => ['placeholder' => "chiffre"],
+            ])
         ;
     }
 
