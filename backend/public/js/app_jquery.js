3eb05f754d4fe8053a40db111a37024cf9f0b95a
Philippe P <44428700+Philpagan@users.noreply.github.com>
2019-03-23 18:31:47 +0100

    création modal générique de confirmation d'opération

--- backend/public/js/app_jquery.js
+++ backend/public/js/app_jquery.js
@@ -1,11 +1,23 @@
 var callAjax = {
   init: function() {
     $(".showModal").click(function(event) {
+        event.preventDefault();
         $(this).siblings(".modal").addClass("is-active");  
       });
       
-      $(".modal-close").click(function(event) {
-        $(this).parents(".modal").removeClass("is-active");
+    $(".modal-close").click(function(event) {
+      $(this).parents(".modal").removeClass("is-active");
+    });
+
+    // pour les Modals de Confirmation / Annulation
+    $(".showModalConfirm").click(function(event) {
+        event.preventDefault();
+        $(this).siblings(".modal").addClass("is-active");  
+      });
+      
+    $(".modal-closeConfirm").click(function(event) {
+      event.preventDefault();
+      $(this).parents(".modal").removeClass("is-active");
     });
   }
 }
