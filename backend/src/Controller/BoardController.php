59d0bc0f331594d5884b9529f354848b61c7ef31
Live session user <furanku13code@gmail.com>
2019-03-26 16:13:32 +0100

    access control et gestion des rôles dans les pages

--- backend/src/Controller/BoardController.php
+++ backend/src/Controller/BoardController.php
@@ -58,24 +58,4 @@ class BoardController extends AbstractController
             'salesUsers' => $salesUsers,
         ]);
     }
-
-    /**
-     * @Route("/list-test", name="list-test")
-     */
-    public function listTest()
-    {
-        return $this->render('tests/list.html.twig', [
-            'page_title' => 'Produit',
-        ]);
-    }
-
-    /**
-     * @Route("/show-test", name="show-test")
-     */
-    public function showTest()
-    {
-        return $this->render('tests/show.html.twig', [
-            'page_title' => 'Produit',
-        ]);
-    }
 }
