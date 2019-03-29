35f59a5ca8f4966f97ba39246e582aa1b2ea6c2b
Pierre-François Picolo <pierre-francois.picolo@hotmail.fr>
2019-03-29 11:30:56 +0100

    *prise en compte du token back pour les appels à l'API

--- frontend/src/store/middlewares/Admin.js
+++ frontend/src/store/middlewares/Admin.js
@@ -17,7 +17,10 @@ import {
 /* TODO : redéfinir l'URL du backend en mode production juste avant la fin */
 
 const axiosUp = axios.create({
-  baseURL: 'http://localhost/Apotheose/crm/backend/public',
+  baseURL: 'http://127.0.0.1:8001/',
+  headers: {
+    Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NTM4NTQ5NjIsImV4cCI6MTU1Mzg1ODU2Miwicm9sZXMiOlsiUk9MRV9BUElVU0VSIl0sInVzZXJuYW1lIjoiY2VyYmVydXMuY3JtLm1haWxlckBnbWFpbC5jb20ifQ.RkzqIFJKXCM-8SGbb9M-NSbTIOsnvS0eYwbZlg_Wm4HdjpG9zCG9WDQ5s0e5ooGF1KtFiSz5pVozx3UqQVpmeOjWHDKjGCM7K2eC3CxtnRoGP7_5LeFAyMZ8eSsK7qjM0U04n0LHLye-1LiHIU7ZMTHgqQlxEHESJtMlYm1ukkK7pTxbZP3l3awTasovkUALsTiY4nsz36m8-FsOgQXzBjBzcm00ZXTftQNMcoS9HHeU4AjT5VJ9T6NckDfQ5HiihkvVLfF9aUcfAxBmC1sEVoiGoWkRFGJ57ggUNZG1d8XQYIe-MOaZBvJWxonnVC8uATWuUuMkoJQFAXFP8vphuRmupAXZUHCAzxNHcYaUXAVijqEN9Lz-jAZQCFTkkYrrf_rJxTTkmx5Wm2isEo7uEit_zYKIKWipOj88Dt7yZUJnQnfRdUjbG9K23gUi8b_DW-c0rEmNd8fp6XZif5azxQRMzXQviAmpYFNFAjAuttEgVcWomHtgotjoVOoGb2dl1oJ9DgJiFGAQhUCBzZa3E4Bwd_ppDu1QzhaDIFg-M9k_VGtST4PYjvIqiW0O-kggs-ti3VgMDKvKwgxNWsZqEIE8gvWF2Oil7o_gDGkwBbuJ34juRDKOPfH87uoY-Z9XXitmyzty6zUzMHHigUiVS8nP436rq2p41ldCrybJJaU',
+  },
 });
 // http://cerberus-crm.space/backend/public
 
@@ -28,7 +31,7 @@ const ajaxAdmin = store => next => (action) => {
     case SEND_LOGIN_REQUEST: {
       const { loginDatas } = action;
       const stringifiedLoginDatas = JSON.stringify(loginDatas);
-      axiosUp.post('/api/login', stringifiedLoginDatas)
+      axiosUp.post('/api/contact/login', stringifiedLoginDatas)
         .then((response) => {
           const { data } = response;
           console.log(data);
--- frontend/src/store/middlewares/Catalog.js
+++ frontend/src/store/middlewares/Catalog.js
@@ -16,7 +16,10 @@ import {
 /* TODO : redéfinir l'URL du backend en mode production juste avant la fin */
 
 const axiosUp = axios.create({
-  baseURL: 'http://localhost/Apotheose/crm/backend/public',
+  baseURL: 'http://127.0.0.1:8001/',
+  headers: {
+    Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NTM4NTQ5NjIsImV4cCI6MTU1Mzg1ODU2Miwicm9sZXMiOlsiUk9MRV9BUElVU0VSIl0sInVzZXJuYW1lIjoiY2VyYmVydXMuY3JtLm1haWxlckBnbWFpbC5jb20ifQ.RkzqIFJKXCM-8SGbb9M-NSbTIOsnvS0eYwbZlg_Wm4HdjpG9zCG9WDQ5s0e5ooGF1KtFiSz5pVozx3UqQVpmeOjWHDKjGCM7K2eC3CxtnRoGP7_5LeFAyMZ8eSsK7qjM0U04n0LHLye-1LiHIU7ZMTHgqQlxEHESJtMlYm1ukkK7pTxbZP3l3awTasovkUALsTiY4nsz36m8-FsOgQXzBjBzcm00ZXTftQNMcoS9HHeU4AjT5VJ9T6NckDfQ5HiihkvVLfF9aUcfAxBmC1sEVoiGoWkRFGJ57ggUNZG1d8XQYIe-MOaZBvJWxonnVC8uATWuUuMkoJQFAXFP8vphuRmupAXZUHCAzxNHcYaUXAVijqEN9Lz-jAZQCFTkkYrrf_rJxTTkmx5Wm2isEo7uEit_zYKIKWipOj88Dt7yZUJnQnfRdUjbG9K23gUi8b_DW-c0rEmNd8fp6XZif5azxQRMzXQviAmpYFNFAjAuttEgVcWomHtgotjoVOoGb2dl1oJ9DgJiFGAQhUCBzZa3E4Bwd_ppDu1QzhaDIFg-M9k_VGtST4PYjvIqiW0O-kggs-ti3VgMDKvKwgxNWsZqEIE8gvWF2Oil7o_gDGkwBbuJ34juRDKOPfH87uoY-Z9XXitmyzty6zUzMHHigUiVS8nP436rq2p41ldCrybJJaU',
+  },
 });
 // http://cerberus-crm.space/backend/public
 
@@ -25,7 +28,7 @@ const ajaxCatalog = store => next => (action) => {
   switch (action.type) {
     case FETCH_CATALOG: {
       const { id } = store.getState().profile;
-      axiosUp.get(`/api/contact/${id}/products`)
+      axiosUp.get(`/api/contact/${id}/product`)
         .then((response) => {
           const { data } = response;
           store.dispatch(fetchSuccess(data, 'catalogList'));
@@ -37,7 +40,7 @@ const ajaxCatalog = store => next => (action) => {
     }
 
     case FETCH_CATEGORIES:
-      axiosUp.get('/api/categories')
+      axiosUp.get('/api/category')
         .then((response) => {
           const { data } = response;
           store.dispatch(fetchSuccess(data, 'categoryList'));
@@ -48,7 +51,7 @@ const ajaxCatalog = store => next => (action) => {
       break;
 
     case FETCH_HOME_PAGE:
-      axiosUp.get('/api/products?isOnHomePage=1')
+      axiosUp.get('/api/product?isOnHomePage=1')
         .then((response) => {
           const { data } = response;
           store.dispatch(fetchSuccess(data, 'articlesOnHomePage'));
