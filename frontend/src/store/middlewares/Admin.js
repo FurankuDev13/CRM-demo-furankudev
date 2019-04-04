62eb94408b461ba3d96fa9d75669cef0ba994413
Pierre-François Picolo <pierre-francois.picolo@hotmail.fr>
2019-04-04 16:00:18 +0200

    changement baseURL pour base en HTTPS + AJOUT DE POPUPS EN CAS DE PROBLÈME

--- frontend/src/store/middlewares/Admin.js
+++ frontend/src/store/middlewares/Admin.js
@@ -22,7 +22,7 @@ import {
 /* TODO : redéfinir l'URL du backend en mode production juste avant la fin */
 
 const axiosUp = axios.create({
-  baseURL: 'http://cerberus-crm.space/backend/public',
+  baseURL: 'https://admin.cerberus-crm.space/',
   headers: {
     Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NTM4NzIxODQsInJvbGVzIjpbIlJPTEVfQVBJVVNFUiJdLCJ1c2VybmFtZSI6ImNlcmJlcnVzLmNybS5tYWlsZXJAZ21haWwuY29tIn0.PJ9ZVb_3ivHOHm6qRh6mhLZcwAu_AT74YoizZ0kItlZg-WaSMsNQPYQ8o-rDL_E1A6beQqtnEaI3w7u98WBl5Nk1RmEfYjVO_5J-P7LSjrJJ-IsabdGoe39jUcdfU2jYcVjdp0dQnc_gDXD83RGvU9brrEBfeH7vuUtGomUP2pRpgumZDWndzSMRWxrkaye8Wc6XeYnt-qNzzpCDLhHBjWb27x-WIqwYqpbqzjZT_wWlaWe8cfuAOP1DoSWGrb8kW9hsxbXuInSOdsK3oCrX45277eySO7OgmxuI4kAAf68JBQZfZtM2_7CAkmFBld_M2rnlBkiF7vCCMyU-Zu7xUWxguWu2cfDgRpMGAmEP1exAUQ20pYI5BZtQADXRtQdkq6_3HwmhirGYrr325IYpRcPrrhTI1_fAetx-U2wzoeMPHaq5dOn-T8K06h6ZOOWwfSsH-YrJa1ZcHlg0dX5zjj360L_gngPiTiW5T0JdWDkrnp0OAA53n_XFTzTedqVn0P6BXNS5iucB-odh72SEzov-fVHIJO5JocLCXPvjFDncevOfNyDEYc-9l_QwfO6ogjoDRQ6vHr_Y4mDxiQhAmFb5kG8K6hd4Ps_UabJs-Sf26lUK2aWAqrGagUH46pDT0zRh0XV7yE3j_SodI8KPb_BIvEd36vxK5fHJkqRoJzU',
   },
@@ -158,8 +158,8 @@ const ajaxAdmin = store => next => (action) => {
         .then(() => {
           popMessage('Votre requête a abouti.', 'success');
         })
-        .catch((error) => {
-          console.log(error);
+        .catch(() => {
+          popMessage('Oups. Suite à un incident, votre demande n\'a pas pu aboutir', 'warning');
         });
       break;
     }
@@ -196,8 +196,8 @@ const ajaxAdmin = store => next => (action) => {
           popMessage('Votre profil a été mis à jour', 'success');
           dispatch(updateProfile(data));
         })
-        .catch((error) => {
-          console.log(error);
+        .catch(() => {
+          popMessage('Suite à une erreur d\'origine inconnue, vos modifications n\'ont pas été prises en compte', 'success');
         });
       break;
     }
